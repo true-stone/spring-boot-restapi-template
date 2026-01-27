98c6205d8a5f9d0c6a5345435604822d0951306f
true-stone <jin_114@naver.com>
2026-01-28 08:13:42 +0900

    feat: Jackson 설정 파일 추가

--- src/main/java/com/example/api/config/JacksonConfig.java
+++ src/main/java/com/example/api/config/JacksonConfig.java
@@ -18,16 +18,22 @@ public class JacksonConfig {
         return builder -> {
             // Jackson 3는 기본이 ISO-8601 문자열이지만, 의도를 명확히 해두면 안전
             builder.disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS);
-            builder.defaultTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+
             // unknown field 무시
             builder.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
             // 빈 직렬화 예외 방지 - 프록시/빈 구조 때문에 “빈 객체” 직렬화 시 예외가 나는 케이스 방지
             builder.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
             // enum 대소문자 허용
             builder.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
 
-            // 선택
-            // builder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+            // 타임존 설정
+            builder.defaultTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+
+            // 선택 사항
+            // builder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);     // 프로퍼티 네이밍 전략
+            // builder.serializationInclusion(JsonInclude.Include.NON_NULL);            // null 응답 포함 여부
         };
     }
 }
